c1893eb4f77104ccd676bd91a0742f8fd8774e2d
abhii14758 <abhibhimani14758@gmail.com>
2024-03-16 19:03:02 +0530

    Updated the image extention from jpeg to jpg

--- portfolio/src/assets/index.js
+++ portfolio/src/assets/index.js
@@ -1,6 +1,6 @@
 import logo from "./images/logo.png"
 import bannerImage from "./images/bannerImage.jpg"
-import projectOne from "./images/projectOne.jpeg"
+import projectOne from "./images/projectOne.jpg"
 
 export {
     logo,
