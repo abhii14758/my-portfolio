2d8f555c2c05e4678bd5f2ed0367796e4715d075
abhii14758 <abhibhimani14758@gmail.com>
2024-03-16 19:21:10 +0530

    Updated the left banner

--- portfolio/src/components/navbar/banner/LeftBanner.js
+++ portfolio/src/components/navbar/banner/LeftBanner.js
@@ -4,24 +4,24 @@ import Typewriter from "typewriter-effect";
 const LeftBanner = () => {
   return (
     <div className='w-full flex flex-col gap-5'>
-          <h4 className='text-lg font-normal '>WELCOME TO MY WORLD</h4>
-          <h1 className='text-6xl font-bold font-titleFont'>Hi, I'm
-          <span className='text-white'> ABHI BHIMANI</span></h1>
-          <h2 className='text-4xl font-bold text-lightText'>Intrested in <span className='text-white'>
-          <Typewriter
+      <h4 className='text-lg font-normal '>WELCOME TO MY WORLD</h4>
+      <h1 className='text-6xl font-bold font-titleFont'>Hi, I'm
+        <span className='text-white'> ABHI BHIMANI</span></h1>
+      <h2 className='text-4xl font-bold text-lightText'>Intrested in <span className='text-white'>
+        <Typewriter
           options={{
-            strings: ['Web Development', 'Machine Learning'],
+            strings: ['Data Science', 'Machine Learning', 'Deep Learning'],
             pauseFor: 1000,
             deleteSpeed: 20,
             autoStart: true,
             loop: true,
           }} /></span></h2>
-          <p className='text-base text-lightText font-bodyFont leading-6 tracking-wide'>
-          3rd year student of computer engineering,
-          sports enthusiast 🏓
-          Always ready to learn new technologies of my domain.
-          </p>
-        </div>
+      <p className='text-base text-lightText font-bodyFont leading-6 tracking-wide'>
+        3rd year student of computer engineering,
+        sports enthusiast 🏓
+        Always ready to learn new technologies of my domain.
+      </p>
+    </div>
   )
 }
 
